c19ebd3b2e7e0f5792d56f665c14a044ab654256
okatoue <omarkatoue@gmail.com>
2024-02-26 21:09:54 -0800

    Update

--- stream/stream/settings.py
+++ stream/stream/settings.py
@@ -115,8 +115,6 @@ USE_TZ = True
 
 STATIC_URL = 'static/'
 
-STATIC_ROOT = BASE_DIR / "staticfiles" / "static"
-
 # Default primary key field type
 # https://docs.djangoproject.com/en/4.0/ref/settings/#default-auto-field
 
